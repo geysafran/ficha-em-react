237d586d9eb6c4b284190623e58c29fd0c8a91e2
vai na web <vainawebcpx@gmail.com>
2019-12-04 12:06:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Ficha from './Ficha'
+
+function App() {
+  return (
+    <div className="App">
+      <Ficha />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ficha.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './App.css';
+
+
+class Ficha extends React.Component {
+
+    constructor(props){
+    super(props);
+
+    this.state = {
+        alunos: [
+            {
+                nome: 'Rhuan',
+                idade: '20 anos',
+                polo: 'Pzr'
+                },
+                {
+                nome: 'Jorge Luis',
+                idade:'58 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'maicon',
+                idade: '23 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'Caroline',
+                idade: '19 anos',
+                polo: 'pzr'
+                },
+                {
+                nome: 'Estefania',
+                idade: '22 anos',
+                polo: 'pzr'
+                },
+                {
+                nome: 'Victor',
+                idade: '24 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'Zaira',
+                idade: '24 anos',
+                polo: 'pzr'
+                },
+                {
+                nome: 'Cleiton',
+                idade: '19 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'Rhaysssa',
+                idade: '21 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'Luan',
+                idade: '18 anos',
+                polo: 'cpx'
+                },
+                {
+                nome: 'Arthur',
+                idade: '24 anos',
+                polo: 'cpx'
+                }
+                 
+            ]
+            }
+        }
+
+
+        listadeNomes = (ev) => {
+            this.state.alunos.map(callback => {
+                console.log(callback.nome, callback.idade, callback.polo) 
+                       return null
+            })
+    }
+    
+    
+
+
+
+
+render (){
+    return (
+        <div className="App">
+
+                
+                <button onClick={this.listadeNomes} className="">Nomes</button>
+                
+            </div>
+
+        
+    )
+}
+
+}
+
+export default Ficha 
